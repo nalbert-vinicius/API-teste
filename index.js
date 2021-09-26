bed5999fa8f3e6f51e22c68d07cbed8d6f721366
Nalbert, Vinicius <rullaxfox@gmail.com>
2021-09-25 22:26:57 -0300

    rotas

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const cors = require('cors')
+const rotas = require('./routes/rotas');
+
+/**
+ * 
+ */
+
 
 const app = express();
 
 app.use(cors());
 
-const userRoute = require('./routes/user');
-
 app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
 
-app.use('/user', userRoute)
+app.use('/user', rotas.userRoute)
 
 
 module.exports = app;
